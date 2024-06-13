c35b99a1dd1f456c0595544767636c0c12e738d2
lucianosantiviago <114786007+lucianosantiviago@users.noreply.github.com>
2024-06-13 09:40:04 -0400

    Add files via upload

---
+++ scripts/project/scriptsInEvents.js
@@ -0,0 +1,24 @@
+
+
+
+const scriptsInEvents = {
+
+	async EventSheet1_Event2(runtime, localVars)
+	{
+		javascript
+		document.addEventListener('keydown', function(event) {
+		    if(event.keyCode === 79) { // 79 es el código que identifica al botón del auricular //
+		        // Trigger a function in Construct 3
+		        runtime.callFunction('BOTON_PRESIONADO');
+				}
+		    if(event.keyCode === 25) { // 79 es el código que identifica al botón del auricular //
+		        // Trigger a function in Construct 3
+		        runtime.callFunction('VOLUMEN');
+		    	}
+				});
+	}
+
+};
+
+self.C3.ScriptsInEvents = scriptsInEvents;
+
